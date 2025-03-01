fcd7acd0adcc7d49eb044102e08acb5b5d322345
Nishant407 <nishant407.ns@gmail.com>
2025-03-01 15:27:47 +0530

    FirstCommit

---
+++ src/main/java/com/example/FirstSpringApp/controller/HelloController.java
@@ -0,0 +1,34 @@
+package com.example.FirstSpringApp.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    // GET request
+    @GetMapping
+    public String sayHello() {
+        return "Hello from BridgeLabz";
+    }
+
+    // POST request
+    @PostMapping
+    public String sayHelloPost(@RequestBody String name) {
+        return "Hello, " + name + " from BridgeLabz";
+    }
+
+    // PUT request
+    @PutMapping("/{name}")
+    public String sayHelloPut(@PathVariable String name) {
+        return "Updated Hello, " + name + " from BridgeLabz";
+    }
+
+    // DELETE request
+    @DeleteMapping("/{name}")
+    public String sayHelloDelete(@PathVariable String name) {
+        return "Goodbye, " + name + " from BridgeLabz";
+
+    }
+
+}
